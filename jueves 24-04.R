326ccd11f0ce7b8c94b7af2603052d4978cbc422
Sebastián Ramírez <sebastian.ramirez.r@ug.uchile.cl>
2025-04-24 11:34:54 -0400

    activar ambiente reproducinle

---
+++ jueves 24-04.R
@@ -0,0 +1,8 @@
+rcompendium::set_credentials(
+  given = "Sebastián",
+  family = "Ramírez",
+  email = "sebastian.ramirez.r@ug.uchile.cl")
+
+rcompendium::new_compendium()
+
+usethis::use_tidy_support()

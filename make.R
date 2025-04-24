08f883f593645480a1a2036dd8491d37e472ada1
Sebastián Ramírez <sebastian.ramirez.r@ug.uchile.cl>
2025-04-24 10:14:28 -0400

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' proyecto reproducibilidad 2: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Sebastián Ramírez \email{sebastian.ramirez.r@ug.uchile.cl}
+#' 
+#' @date 2025/04/24
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
